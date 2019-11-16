d18ca31a50019c5467a4279481d65df79e2ced41
ruanjiayou <ruanjiayou123@gmail.com>
2019-11-16 10:41:54 +0700

    init

---
+++ precache-manifest.96931b4951bb34d306f9d83894e98ab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a741ebde55af4cdbfcb2e6525a9f618b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14d5954d9cb26486c9bd",
+    "url": "/static/css/2.6f60e7f3.chunk.css"
+  },
+  {
+    "revision": "4b39e807fb1499495f7d",
+    "url": "/static/css/main.7a01ee45.chunk.css"
+  },
+  {
+    "revision": "14d5954d9cb26486c9bd",
+    "url": "/static/js/2.75f1ecde.chunk.js"
+  },
+  {
+    "revision": "4b39e807fb1499495f7d",
+    "url": "/static/js/main.a51864e9.chunk.js"
+  },
+  {
+    "revision": "66ff345f11d0a39b8eac",
+    "url": "/static/js/runtime-main.83206213.js"
+  }
+]);
---
+++ precache-manifest.e16bde0e1092c4cc334bb80ae999cc23.js
@@ -0,0 +1,33 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a741ebde55af4cdbfcb2e6525a9f618b",
+    "url": "/index.html"
+  },
+  {
+    "url": "/precache-manifest.96931b4951bb34d306f9d83894e98ab5.js"
+  },
+  {
+    "revision": "c960acd43361389782231c0003009092",
+    "url": "/service-worker.js"
+  },
+  {
+    "revision": "14d5954d9cb26486c9bd",
+    "url": "/static/css/2.6f60e7f3.chunk.css"
+  },
+  {
+    "revision": "4b39e807fb1499495f7d",
+    "url": "/static/css/main.7a01ee45.chunk.css"
+  },
+  {
+    "revision": "14d5954d9cb26486c9bd",
+    "url": "/static/js/2.75f1ecde.chunk.js"
+  },
+  {
+    "revision": "4b39e807fb1499495f7d",
+    "url": "/static/js/main.a51864e9.chunk.js"
+  },
+  {
+    "revision": "66ff345f11d0a39b8eac",
+    "url": "/static/js/runtime-main.83206213.js"
+  }
+]);
